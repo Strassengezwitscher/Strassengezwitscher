a857b6273b00fb6b39ddc3a6f7f58a7f82ed3970
Max Bothe <mathebox@gmail.com>
2016-04-26 13:44:58 +0200

    Set default values for events and mapobjects.

--- strassengezwitscher/events/models.py
+++ strassengezwitscher/events/models.py
@@ -5,9 +5,9 @@ from mapobjects.models import MapObject
 
 class Event(MapObject):
     date = models.DateField()
-    repetition_cycle = models.CharField(max_length=50)
+    repetition_cycle = models.CharField(max_length=50, default='unbekannter Rhythmus')
     organizer = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
-    link_to_event = models.URLField()
-    counter_event = models.BooleanField()
+    link_to_event = models.URLField(blank=True, null=True)
+    counter_event = models.BooleanField(default=False)
     coverage = models.BooleanField()
--- strassengezwitscher/mapobjects/models.py
+++ strassengezwitscher/mapobjects/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class MapObject(models.Model):
     name = models.CharField(max_length=100)
-    active = models.BooleanField()
+    active = models.BooleanField(default=False)
     location_long = models.DecimalField(max_digits=9, decimal_places=6)
     location_lat = models.DecimalField(max_digits=9, decimal_places=6)
