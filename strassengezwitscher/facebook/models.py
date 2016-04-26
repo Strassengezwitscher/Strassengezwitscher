a5fd30f950b394471fc84dc3d23f44d42930e43e
Max Bothe <mathebox@gmail.com>
2016-04-26 13:08:43 +0200

    Add link between facebook page and event.

--- strassengezwitscher/facebook/models.py
+++ strassengezwitscher/facebook/models.py
@@ -2,9 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 from mapobjects.models import MapObject
+from events.models import Event
+
 
 class FacebookPage(MapObject):
     link_to_facebook = models.URLField()
+    events = models.ManyToManyField(Event)
 
 
 class FacebookLikeStatistic(models.Model):
