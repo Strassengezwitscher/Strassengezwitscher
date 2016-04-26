36700f0f2d59f8e4bc9108d8ffb0352e1e5d3db6
Max Bothe <mathebox@gmail.com>
2016-04-26 17:25:10 +0200

    Add first landing page with map.
    - Create app for map.
    - Add base template.

--- strassengezwitscher/strassengezwitscher/settings.py
+++ strassengezwitscher/strassengezwitscher/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'mapobjects.apps.MapobjectsConfig',
     'facebook.apps.FacebookConfig',
     'events.apps.EventsConfig',
+    'map.apps.MapConfig',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'strassengezwitscher.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
