59421f2106d6797a666b680476bd8582c5d670b6
Max Bothe <mathebox@gmail.com>
2016-05-02 20:53:01 +0200

    Remove twitter redirect page.
    - Show simple map page instead.

--- strassengezwitscher/map/views.py
+++ strassengezwitscher/map/views.py
@@ -4,7 +4,3 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
-
-
-def twitter_redirect(request):
-    return render(request, 'twitter_redirect.html')
