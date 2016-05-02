01ea80a96a6319576f5411332bdeffa6e6a6ca91
Max Bothe <mathebox@gmail.com>
2016-05-02 20:44:54 +0200

    Add redirect page to twitter to show wip state.

--- strassengezwitscher/map/urls.py
+++ strassengezwitscher/map/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.twitter_redirect, name='twitter_redirect'),
+    url(r'^map$', views.index, name='index'),
 ]
--- strassengezwitscher/map/views.py
+++ strassengezwitscher/map/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def twitter_redirect(request):
+    return render(request, 'twitter_redirect.html')
