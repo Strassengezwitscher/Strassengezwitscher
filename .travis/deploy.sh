dbcc0e7e30c59cc968834e668af689a817cce58c
Matthias Bastian <dev@matthias-bastian.de>
2016-05-04 19:50:06 +0200

    run deployment only when testing against python 2.7

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,9 +2,11 @@
 
 DEPLOY_BRANCHES=(master develop)
 
-for i in "${DEPLOY_BRANCHES[@]}"; do
-    if [[ "$i" = $TRAVIS_BRANCH ]]; then
-        ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no deploy@5.45.110.119 sudo ./deploy_$TRAVIS_BRANCH.sh
-        break
-    fi
-done
+if [[ $TRAVIS_PYTHON_VERSION = "2.7" ]]; then
+    for i in "${DEPLOY_BRANCHES[@]}"; do
+        if [[ "$i" = $TRAVIS_BRANCH ]]; then
+            ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no deploy@5.45.110.119 sudo ./deploy_$TRAVIS_BRANCH.sh
+            break
+        fi
+    done
+fi
