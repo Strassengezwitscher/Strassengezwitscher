22c0c9008d0f1278ffd614765cca1f79d5e46230
Matthias Bastian <dev@matthias-bastian.de>
2016-05-04 18:46:23 +0200

    getting closer...

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,7 @@ DEPLOY_BRANCHES=(master develop)
 
 for i in "${DEPLOY_BRANCHES[@]}"; do
     if [[ "$i" = $TRAVIS_BRANCH ]]; then
-        ssh -i deploy_key.pem -oStrictHostKeyChecking=no deploy@5.45.110.119 sudo ./deploy_$TRAVIS_BRANCH.sh
+        ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no deploy@5.45.110.119 sudo ./deploy_$TRAVIS_BRANCH.sh
         break
     fi
 done
