65a8facce11f4de7e4f73856e7578669c42a5f79
Matthias Bastian <dev@matthias-bastian.de>
2016-05-05 14:56:47 +0200

    limited travis rights to its deployment tasks

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,9 @@ DEPLOY_BRANCHES=(master develop)
 if [[ $TRAVIS_PYTHON_VERSION = "2.7" ]]; then
     for i in "${DEPLOY_BRANCHES[@]}"; do
         if [[ "$i" = $TRAVIS_BRANCH ]]; then
-            ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no deploy@5.45.110.119 sudo ./deploy_$TRAVIS_BRANCH.sh
+        	openssl aes-256-cbc -K $encrypted_423a58ca66b3_key -iv $encrypted_423a58ca66b3_iv -in deploy_key.pem.enc -out deploy_key.pem -d
+			chmod 600 deploy_key.pem
+            ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no travis@5.45.110.119 sudo ./deploy_$TRAVIS_BRANCH.sh
             break
         fi
     done
