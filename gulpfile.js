16cd3c6e5a3e421a2d5f65b684c524e7c42893a6
Max Bothe <mathebox@gmail.com>
2016-05-09 15:04:04 +0200

    Compile scss files to css.
    - Compile files to separate directory.
    - Add gulp task to compile scss.
    - Add gulp watch task for scss.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
+'use strict';
 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var sass_path = './strassengezwitscher/**/css/*.scss'
 var static_npm_file_paths = [
     'node_modules/bootstrap/dist/css/bootstrap.min.css',
     'node_modules/bootstrap/dist/css/bootstrap.min.css.map',
@@ -8,9 +12,21 @@ var static_npm_file_paths = [
 ];
 
 gulp.task('copy_static_npm_files', function() {
-    gulp.src(static_npm_file_paths, {base: 'node_modules/'})
+    return gulp.src(static_npm_file_paths, {base: 'node_modules/'})
         .pipe(gulp.dest('strassengezwitscher/static/lib/'));
 });
+
+gulp.task('compile_sass', function() {
+    return gulp.src(sass_path)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('strassengezwitscher/static/compiled/'));
+});
+
+gulp.task('watch_sass', ['compile_sass'], function() {
+    return gulp.watch(sass_path, ['compile_sass']);
+});
+
+gulp.task('watch', ['watch_sass']);
 
 gulp.task('default', function() {
   // place code for your default task here
