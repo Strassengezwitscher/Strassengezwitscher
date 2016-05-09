88d8b952f3485c8d82c2c2b0282abdaaed1fd156
Max Bothe <mathebox@gmail.com>
2016-05-09 12:27:54 +0200

    Add gulp task to copy static files from npm directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 
+var static_npm_file_paths = [
+    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    'node_modules/bootstrap/dist/css/bootstrap.min.css.map'
+];
+
+gulp.task('copy_static_npm_files', function() {
+    gulp.src(static_npm_file_paths, {base: 'node_modules/'})
+        .pipe(gulp.dest('strassengezwitscher/static/lib/'));
+});
+
 gulp.task('default', function() {
   // place code for your default task here
 });
--- strassengezwitscher/strassengezwitscher/settings.py
+++ strassengezwitscher/strassengezwitscher/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "../node_modules"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/lib"),)
 
 # Create a localsettings.py if you want to locally override settings
 # and don't want the changes to appear in 'git status'.
