d26778097f64cf00a5503cefd09fe814be140c2b
Max Bothe <mathebox@gmail.com>
2016-05-09 13:36:30 +0200

    Load google map on controller init.

--- strassengezwitscher/map/static/js/controllers.js
+++ strassengezwitscher/map/static/js/controllers.js
@@ -20,4 +20,25 @@ sgApp.controller('MapCtrl', function ($scope) {
             'text': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,...'
         }
     ];
+
+    function init() {
+        /* position Amsterdam */
+        var latlng = new google.maps.LatLng(52.3731, 4.8922);
+
+        var mapOptions = {
+          center: latlng,
+          scrollWheel: false,
+          zoom: 13
+        };
+
+        var marker = new google.maps.Marker({
+          position: latlng,
+          url: '/',
+          animation: google.maps.Animation.DROP
+        });
+
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        marker.setMap(map);
+    };
+    init();
 });
