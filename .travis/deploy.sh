fe5f04a045c92406f46ce0d199332fb7898be5e9
Matthias Bastian <dev@matthias-bastian.de>
2016-05-18 03:38:56 +0200

    travis is prepared for improved deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,7 +7,7 @@ if [[ $TRAVIS_PYTHON_VERSION = "2.7" ]]; then
         if [[ "$i" = $TRAVIS_BRANCH ]]; then
         	openssl aes-256-cbc -K $encrypted_423a58ca66b3_key -iv $encrypted_423a58ca66b3_iv -in deploy_key.pem.enc -out deploy_key.pem -d
 			chmod 600 deploy_key.pem
-            ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no travis@5.45.110.119 sudo ./deploy_$TRAVIS_BRANCH.sh
+            ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no travis@5.45.110.119 sudo -H -u deploy /home/deploy/deploy_$TRAVIS_BRANCH.sh
             break
         fi
     done
