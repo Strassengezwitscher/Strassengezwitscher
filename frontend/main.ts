42a83b1db4324091ac4cc382e2aedd6aab8e59c0
Max Bothe <mathebox@gmail.com>
2016-05-18 13:08:06 +0200

    Make use of Angular2 routing system.

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,7 +1,8 @@
 /// <reference path="../typings/browser.d.ts" />
 
 import { bootstrap } from "@angular/platform-browser-dynamic";
+import { ROUTER_PROVIDERS } from "@angular/router";
 
 import { StrassengezwitscherComponent } from "./sg.component";
 
-bootstrap(StrassengezwitscherComponent);
+bootstrap(StrassengezwitscherComponent, [ROUTER_PROVIDERS]);
--- frontend/sg.component.ts
+++ frontend/sg.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from "@angular/core";
+import { Routes, Router, ROUTER_DIRECTIVES } from "@angular/router";
+
 import { NavBarComponent } from "./navbar.component";
 import { MapComponent } from "./map.component";
 
 @Component({
     selector: "sg-app",
     templateUrl: "sg.component.html",
-    directives: [NavBarComponent, MapComponent]
+    directives: [
+        ROUTER_DIRECTIVES,
+        NavBarComponent
+    ]
 })
-export class StrassengezwitscherComponent {}
+@Routes([
+    { path: "/", component: MapComponent }
+])
+export class StrassengezwitscherComponent implements OnInit {
+    constructor(private router: Router) {}
+
+    ngOnInit() {
+        this.router.navigate(["/"]);
+    }
+}
