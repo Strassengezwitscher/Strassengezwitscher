29278d47567fcac0013bbcd4700a65a1f82a5b9d
Max Bothe <mathebox@gmail.com>
2016-05-24 19:07:50 +0200

    Add simple html template for admin pages.

--- strassengezwitscher/strassengezwitscher/urls.py
+++ strassengezwitscher/strassengezwitscher/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^intern/', include('mapobjects.urls', namespace='mapobjects')),
     url(r'^', include('map.urls', namespace='map')),
 ]
