d34c2ce1be436f348bacea2a2b5fd6cabf253610
Benjamin Reissaus <benjamin.reissaus@student.hpi.uni-potsdam.de>
2016-06-04 15:31:13 +0200

    make mapobjects available under /api/mapobjects

--- strassengezwitscher/strassengezwitscher/urls.py
+++ strassengezwitscher/strassengezwitscher/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('map.urls', namespace='map')),
-    url(r'^mapobjects/', include('mapobjects.urls')),
+    url(r'^api/mapobjects/', include('mapobjects.urls')),
 ]
