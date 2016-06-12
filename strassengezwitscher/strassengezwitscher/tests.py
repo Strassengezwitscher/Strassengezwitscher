3ca17ed50d1124d6590d7f4c172eeefbfaeb49dc
Max Bothe <mathebox@gmail.com>
2016-06-12 15:11:55 +0200

    Test serving of frontend page.

---
+++ strassengezwitscher/strassengezwitscher/tests.py
@@ -0,0 +1,11 @@
+# pylint: disable=invalid-name,too-many-public-methods
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+
+class StrassengezwitscherTests(TestCase):
+    def test_serves_angular_tag(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<sg-app>', response.content)
+        self.assertIn(b'</sg-app>', response.content)
