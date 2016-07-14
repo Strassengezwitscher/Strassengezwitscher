d117e80fd0c2b9b9dae68d61cc289965a1cdc68b
Max Bothe <mathebox@gmail.com>
2016-07-14 16:54:30 +0200

    Try to fix repo root problem.

--- gulpfile.js
+++ gulpfile.js
@@ -170,7 +170,7 @@ if (!argv.production) {
     });
 
     gulp.task('coverage:python', function() {
-        var command = 'coverage run --source="." strassengezwitscher/manage.py test strassengezwitscher';
+        var command = 'coverage run --source="./strassengezwitscher" strassengezwitscher/manage.py test strassengezwitscher';
         exec(command, function (err, stdout, stderr) {
             console.log(stdout);
             console.log(stderr);
