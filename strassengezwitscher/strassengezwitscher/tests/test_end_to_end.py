bf225dcfbeb47aa1e549e6576a21ac81457f1a87
Max Bothe <mathebox@gmail.com>
2016-07-20 00:30:50 +0200

    Use timeouts.

--- strassengezwitscher/strassengezwitscher/tests/test_end_to_end.py
+++ strassengezwitscher/strassengezwitscher/tests/test_end_to_end.py
@@ -1,23 +1,27 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class StrassengezwitscherSeleniumTests(StaticLiveServerTestCase):
+    TIME_OUT = 10
+
     @classmethod
     def setUpClass(cls):
         super(StrassengezwitscherSeleniumTests, cls).setUpClass()
-        cls.selenium = WebDriver()
+        cls.driver = WebDriver()
+        cls.wait = WebDriverWait(cls.driver, StrassengezwitscherSeleniumTests.TIME_OUT)
 
     @classmethod
     def tearDownClass(cls):
-        cls.selenium.quit()
+        cls.driver.quit()
         super(StrassengezwitscherSeleniumTests, cls).tearDownClass()
 
     def test_map(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/'))
-        self.selenium.find_element_by_tag_name('sg-map')
+        self.driver.get('%s%s' % (self.live_server_url, '/'))
+        self.wait.until(lambda driver: driver.find_elements_by_tag_name('sg-map'))
 
     def test_contact(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/contact'))
-        p = self.selenium.find_element_by_tag_name('p')
-        self.assertTrue(p.text == 'Insert contact form here.')
+        self.driver.get('%s%s' % (self.live_server_url, '/contact'))
+        results = self.wait.until(lambda driver: driver.find_elements_by_tag_name('p'))
+        [self.assertTrue(result.text == 'Insert contact form here.') for result in results]
