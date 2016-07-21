0220c3ba819c0f8ed4fd825da7304a5a1d8f81f7
Matthias Bastian <dev@matthias-bastian.de>
2016-07-21 23:32:34 +0200

    Make e2e-tests find phantomjs executable

--- gulpfile.js
+++ gulpfile.js
@@ -148,7 +148,7 @@ if (!argv.production) {
     });
 
     gulp.task('test:e2e', function(done) {
-        var command = './node_modules/casperjs/bin/casperjs test ' + config.e2e.files;
+        var command = 'PHANTOMJS_EXECUTABLE=./node_modules/phantomjs-prebuilt/bin/phantomjs ./node_modules/casperjs/bin/casperjs test ' + config.e2e.files;
         var casper = exec(command, function (err, stdout, stderr) {
             console.log(stdout);
             console.log(stderr);
