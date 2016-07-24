e5a2a895fc0ed74f7f146794053d2ebc9ce21b1a
Christopher Schmidt <christopher.schmidt1989@gmail.com>
2016-07-24 11:48:46 +0200

    success reroutes to map

--- frontend/contact.component.ts
+++ frontend/contact.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component }      from "@angular/core";
+import { Router }         from "@angular/router";
 // TODO (Chris) integrate Captcha properly: import {ReCaptchaComponent} from 'angular2-recaptcha/angular2-recaptcha';
 
 import { ContactService } from "./contact.service";
-import { Contact } from "./contact";
+import { Contact }        from "./contact";
 
 @Component({
     selector: "sg-contact",
@@ -12,13 +13,19 @@ import { Contact } from "./contact";
 })
 export class ContactComponent {
     public contact = new Contact("", "", "", "", null, null, null);
-    constructor( private contactService: ContactService) {}
+    constructor( private contactService: ContactService, private router: Router) {}
 
     public onFileChange(event) {
         this.contact.files = event.srcElement.files;
     }
 
     public onSubmit() {
-        this.contactService.addContactData(this.contact).subscribe();
+        this.contactService.addContactData(this.contact).subscribe((data) =>
+                                            this.router.navigate([""]), (err) => this.displayError(err));
+    }
+
+    private displayError(err: any) {
+        // TODO (Chris) handle error codes
+        console.log(err);
     }
 }
