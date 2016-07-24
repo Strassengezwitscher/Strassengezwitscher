4b0efb1fc573106a6a95bd553ad912515cf7dbbf
Christopher Schmidt <christopher.schmidt1989@gmail.com>
2016-07-25 01:32:42 +0200

    added test cases for disabled/enabled submit button, due to required and email validation

--- e2e/test_contact.js
+++ e2e/test_contact.js
@@ -14,7 +14,7 @@ casper.test.begin('Testing Contact (User Frontend fields available)', 11, functi
         test.assertExists('input#contact-journalist[type=checkbox]','Input Checkbox for journalist');
         test.assertExists('input#contact-confidential[type=checkbox]','Input Checkbox for confidential');
         test.assertExists('input#contact-file[type=file]','File Input for files');
-        test.assertExists('button#contact-submit-button[type=submit]','Submit button exists')
+        test.assertExists('button#contact-submit-button[type=submit]:disabled','Disabled submit button exists')
 
     }, function timeout() {
         test.fail('Could not find element with tag "sg-contact"');
@@ -38,6 +38,40 @@ casper.test.begin('Testing Contact (Name max length)', 3, function(test) {
     }, function timeout() {
         test.fail('Could not find element with tag "sg-contact"');
     });
+    casper.run(function(){
+        test.done();
+    })
+});
+
+
+casper.test.begin('Testing Contact (Submission enabled/disabled)', 5, function(test) {
+    casper.start(settings.contactUrl);
+
+    casper.waitUntilVisible('sg-contact', function then(){
+        // test case for disabled button with missing subject
+        this.sendKeys('#contact-message','Tolle Nachricht');
+        test.assertExists('button#contact-submit-button:disabled', 'disabled button, missing subject');
+        // test case for disabled button with missing message
+        this.sendKeys('#contact-message','', {reset : true});
+        this.sendKeys('#contact-subject','Betreff');
+        test.assertExists('button#contact-submit-button:disabled', 'disabled button, missing message');
+        // test case for enabled button with subject and message, rest empty
+        this.sendKeys('#contact-subject','Betreff');
+        this.sendKeys('#contact-message','Tolle Nachricht');
+        test.assertExists('button#contact-submit-button:enabled', 'enabled button')
+        // test case for disabled button with wrong email
+        this.sendKeys('#contact-email','this@doesNotWork');
+        test.assertExists('button#contact-submit-button:disabled', 'disabled button, wrong email');
+        // test case for enabled button with subject and message and correct email
+        this.sendKeys('#contact-subject','Betreff');
+        this.sendKeys('#contact-message','Tolle Nachricht');
+        this.sendKeys('#contact-email','this@doesWork.com', {reset : true});
+        test.assertExists('button#contact-submit-button:enabled', 'enabled button')
+    }, function timeout() {
+        test.fail('Could not find element with tag "sg-contact"');
+    });
+
+
     casper.run(function(){
         test.done();
     })
