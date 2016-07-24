1dd5fe9084b016b8ca8085448451270767567743
Christopher Schmidt <christopher.schmidt1989@gmail.com>
2016-07-24 20:08:06 +0200

    some test cases for contact

--- e2e/test_contact.js
+++ e2e/test_contact.js
@@ -1,11 +1,12 @@
 var settings = require('./settings')();
 
-casper.test.begin('Testing Contact (User Fronted)', 2, function(test) {
+casper.test.begin('Testing Contact (User Frontend initial)', 3, function(test) {
     casper.start(settings.contactUrl);
 
     casper.waitUntilVisible('sg-contact', function then(){
         test.assertVisible('sg-contact', 'Contact form is visible');
-        test.assertTextExists('Insert contact form here.', 'Does show wip text');
+        test.assertExists('#contact-form', 'The contact form is available');
+        test.assertNotVisible('#contact-error-message', 'Error message not visible');
     }, function timeout() {
         test.fail('Could not find element with tag "sg-contact"');
     });
@@ -14,3 +15,28 @@ casper.test.begin('Testing Contact (User Fronted)', 2, function(test) {
         test.done();
     })
 });
+
+
+casper.test.begin('Testing Contact (Name max length)', 3, function(test) {
+    casper.start(settings.contactUrl);
+
+    casper.waitUntilVisible('sg-contact', function then(){
+        this.sendKeys('#contact-name', 'Peter');
+        test.assertSelectorHasText('#contact-name', 'Peter');
+    }, function timeout() {
+        test.fail('Could not find element with tag "sg-contact"');
+    });
+
+    casper.waitUntilVisible('sg-contact', function then(){
+        this.sendKeys('#contact-name', 'PeterPeterPeterPeterPeterPeterPeterPeterPeterPeterZuViel');
+        test.assertSelectorDoesntHaveText('#contact-name', 'PeterPeterPeterPeterPeterPeterPeterPeterPeterPeterZuViel');
+        test.assertSelectorHasText('#contact-name', 'PeterPeterPeterPeterPeterPeterPeterPeterPeterPeter');
+    }, function timeout() {
+        test.fail('Could not find element with tag "sg-contact"');
+    });
+
+    casper.run(function(){
+        test.done();
+    })
+});
+
