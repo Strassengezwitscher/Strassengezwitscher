595bcdc3cfced05a78451ca59d6026fdfaecd345
Max Bothe <mathebox@gmail.com>
2016-07-24 16:41:42 +0200

    Adapt tests and fixtures to migration changes.
    - Remove mapobejcts app.

--- strassengezwitscher/events/tests/test_views.py
+++ strassengezwitscher/events/tests/test_views.py
@@ -13,7 +13,7 @@ class EventViewTests(TestCase):
         response = self.client.get(reverse('events:list'))
         self.assertEqual(response.status_code, 200)
         self.assertIn('events', response.context)
-        self.assertQuerysetEqual(response.context['events'], [repr(event) for event in Event.objects.all()])
+        self.assertEqual(list(response.context['events']), list(Event.objects.all()))
 
     def test_post_list_view_not_allowed(self):
         response = self.client.post(reverse('events:list'))
@@ -55,7 +55,7 @@ class EventViewTests(TestCase):
             'coverage': False,
         }
         response = self.client.post(reverse('events:create'), data, follow=True)
-        self.assertRedirects(response, reverse('events:detail', kwargs={'pk': 2}))
+        self.assertRedirects(response, reverse('events:detail', kwargs={'pk': 3}))
 
     def test_post_create_view_no_data(self):
         response = self.client.post(reverse('events:create'))
