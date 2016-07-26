631ec5794913c8c206618c666a5ee664e3924ee5
Christopher Schmidt <christopher.schmidt1989@gmail.com>
2016-07-26 21:37:10 +0200

    added tests for contact

---
+++ frontend/contact.component.spec.ts
@@ -0,0 +1,47 @@
+import { ContactComponent }   from "./contact.component";
+import { ContactService }     from "./contact.service";
+import { Contact }            from "./contact";
+
+describe("ContactComponent", () => {
+
+    beforeEach(function() {
+        this.cc = new ContactComponent(new ContactService(), null);
+    });
+
+    it("check if error message is set", function () {
+        this.cc.displayError({"status": 500, "error": {"errors": "wrong"}});
+        expect(this.cc.contactErrorMessage).toEqual("Fehler bei der Kontaktaufnahme: \nInterner Fehler, wrong");
+    });
+
+    it("check if error message is set (status 400)", function () {
+        this.cc.displayError({"status": 400, "error": {"errors": {"name": "exceeds 50", "email": "wrong"}}});
+        expect(this.cc.contactErrorMessage).toEqual("Fehler bei der Kontaktaufnahme: \n" +
+            "name: exceeds 50 \nemail: wrong \n");
+    });
+
+    it("check if error message is cleared", function () {
+        this.cc.displayError({"status": 500, "error": {"errors": "wrong"}});
+        this.cc.clearError();
+        expect(this.cc.contactErrorMessage).toEqual("");
+    });
+
+    it("check on file change too long file name", function () {
+        this.cc.onFileChange({"srcElement": {"files": [{"name":
+            "12345678901234567890123456789012345678901234567890.txt"}]}});
+        expect(this.cc.filesValid).toEqual(false);
+        expect(this.cc.contactErrorMessage).toEqual("Name des Anhangs '" +
+                   "12345678901234567890123456789012345678901234567890.txt' zu lang (maximal 50 Zeichen)\n");
+    });
+
+    it("check on file change correct filename", function () {
+        this.cc.onFileChange({"srcElement": {"files": [{"name": "1234567890.txt"}]}});
+        expect(this.cc.filesValid).toEqual(true);
+        expect(this.cc.uploads).toEqual([{"name": "1234567890.txt"}]);
+    });
+
+    it("check initilization", function() {
+        expect(this.cc.contact).toEqual(new Contact("", "", "", "", null, null));
+        expect(this.cc.filesValid).toEqual(true);
+    });
+
+});
