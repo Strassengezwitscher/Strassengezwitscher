c5f3c29cfe64c5aafda049dc5db6b36ec1aad5ef
Christopher Schmidt <christopher.schmidt1989@gmail.com>
2016-07-26 18:55:08 +0200

    include check for max file name length in frontend

--- frontend/contact.component.ts
+++ frontend/contact.component.ts
@@ -17,13 +17,29 @@ export class ContactComponent {
     private contactErrorMessage: string;
     private contact: Contact;
     private uploads: FileList;
+    private maxFileNameLength = 50;
+    private filesValid = true;
 
     constructor( private contactService: ContactService, private router: Router) {
         this.contact = new Contact("", "", "", "", null, null);
+        this.filesValid = true;
     }
 
     public onFileChange(event) {
-        this.uploads = event.srcElement.files;
+        let errorMessage = "";
+        for (let i = 0; i < event.srcElement.files.length; ++i) {
+            if (event.srcElement.files[i].name.length > this.maxFileNameLength) {
+                errorMessage += "Name des Anhangs '" + event.srcElement.files[i].name +
+                                "' zu lang (maximal 50 Zeichen)\n";
+            }
+        }
+        if (errorMessage) {
+            this.filesValid = false;
+            this.contactErrorMessage = errorMessage;
+        } else {
+            this.filesValid = true;
+            this.uploads = event.srcElement.files;
+        }
     }
 
     public onSubmit() {
