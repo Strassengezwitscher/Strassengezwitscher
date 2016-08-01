6faf9c9faa79331f09a3a1cffafd2dddebf0b781
Matthias Bastian <piepmatz@users.noreply.github.com>
2016-08-01 15:45:09 +0200

    make travis not deploy the branch when simulating the result of a PR (#120)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,13 +2,15 @@
 
 DEPLOY_BRANCHES=(master develop)
 
-if [[ $TRAVIS_PYTHON_VERSION = "2.7" ]]; then
-    for i in "${DEPLOY_BRANCHES[@]}"; do
-        if [[ "$i" = $TRAVIS_BRANCH ]]; then
-        	openssl aes-256-cbc -K $encrypted_423a58ca66b3_key -iv $encrypted_423a58ca66b3_iv -in deploy_key.pem.enc -out deploy_key.pem -d
-			chmod 600 deploy_key.pem
-            ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no travis@5.45.110.119 sudo -H -u deploy /home/deploy/deploy_$TRAVIS_BRANCH.sh
-            break
-        fi
-    done
+if [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    if [[ "$TRAVIS_PYTHON_VERSION" = "2.7" ]]; then
+        for i in "${DEPLOY_BRANCHES[@]}"; do
+            if [[ "$i" = $TRAVIS_BRANCH ]]; then
+            	openssl aes-256-cbc -K $encrypted_423a58ca66b3_key -iv $encrypted_423a58ca66b3_iv -in deploy_key.pem.enc -out deploy_key.pem -d
+    			chmod 600 deploy_key.pem
+                ssh -i ./deploy_key.pem -oStrictHostKeyChecking=no travis@5.45.110.119 sudo -H -u deploy /home/deploy/deploy_$TRAVIS_BRANCH.sh
+                break
+            fi
+        done
+    fi
 fi
