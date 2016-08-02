87d6de9d86415408ead1457e8e1e02e6ed4d861b
Matthias Bastian <dev@matthias-bastian.de>
2016-08-02 13:44:07 +0200

    unrelated: silence a linter-warning

--- strassengezwitscher/strassengezwitscher/settings/production.py
+++ strassengezwitscher/strassengezwitscher/settings/production.py
@@ -10,7 +10,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")  # noqa
 
 ######################################################################################################
 #                                           Email settings
