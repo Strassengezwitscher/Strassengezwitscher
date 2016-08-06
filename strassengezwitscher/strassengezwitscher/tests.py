6788d5e6276d78f8fbc144951c5bfcfe11387434
Benjamin Reissaus <benjamin.reissaus@student.hpi.uni-potsdam.de>
2016-08-06 19:14:34 +0200

    add tests for MapObjects model

--- strassengezwitscher/strassengezwitscher/tests.py
+++ strassengezwitscher/strassengezwitscher/tests.py
@@ -1,8 +1,10 @@
 from django.urls import reverse
 from django.test import TestCase
+from rest_framework.exceptions import ValidationError
 
 from strassengezwitscher.models import MapObject
 
+import json
 
 class StrassengezwitscherTests(TestCase):
     def test_serves_angular_tag(self):
@@ -20,3 +22,191 @@ class MapObjectModelTests(TestCase):
     def test_string_representation(self):
         obj = MapObject(name='Test')
         self.assertEqual(str(obj), 'Test')
+
+    """
+        Test is_between_longitudes()
+    """
+    #
+    # Case 1: Min' < 'Max' --> no shift of points
+    #
+
+    # 'H' [left of] 'Min' [left of] 'Max'
+    def test_is_between_longitudes_a(self):
+        min_long = 100
+        location_long = 0
+        max_long = 150
+
+        obj = MapObject(location_long=location_long)
+        self.assertFalse(obj.is_between_longitudes(min_long=min_long, max_long=max_long))
+
+    # 'Min' [left of] 'H' [left of] 'Max'
+    def test_is_between_longitudes_b(self):
+        min_long = 100
+        location_long = 130
+        max_long = 150
+
+        obj = MapObject(location_long=location_long)
+        self.assertTrue(obj.is_between_longitudes(min_long=min_long, max_long=max_long))
+
+    # 'Min' [left of] 'Max' [left of] 'H'
+    def test_is_between_longitudes_c(self):
+        min_long = 100
+        location_long = 170
+        max_long = 150
+
+        obj = MapObject(location_long=location_long)
+        self.assertFalse(obj.is_between_longitudes(min_long=min_long, max_long=max_long))
+
+    #
+    # Case 2: 'Min' > 'Max'
+    # --> 'Min' [left of] +180/-180 [left of] 'Max'
+    #
+
+    # 'H' [left of] 'Min' [left of] +180/-180 [left of] 'Max'
+    # --> 'Max' has to be shifted
+    def test_is_between_longitudes_d(self):
+        min_long = 45
+        location_long = 20
+        max_long = -160
+
+        obj = MapObject(location_long=location_long)
+        self.assertFalse(obj.is_between_longitudes(min_long=min_long, max_long=max_long))
+
+    # 'Min' [left of] 'H' [left of] +180/-180 [left of] 'Max'
+    # --> 'Max' has to be shifted
+    def test_is_between_longitudes_e(self):
+        min_long = 45
+        location_long = 60
+        max_long = -160
+
+        obj = MapObject(location_long=location_long)
+        self.assertTrue(obj.is_between_longitudes(min_long=min_long, max_long=max_long))
+
+    # 'Min' [left of] +180/-180 [left of] 'H' [left of] 'Max'
+    # --> 'Max' and 'H' have to be shifted
+    def test_is_between_longitudes_f(self):
+        min_long = 45
+        location_long = -170
+        max_long = -160
+
+        obj = MapObject(location_long=location_long)
+        self.assertTrue(obj.is_between_longitudes(min_long=min_long, max_long=max_long))
+
+    # 'Min' [left of] +180/-180 [left of] 'Max' [left of] 'H'
+    # --> 'Max' and 'H' have to be shifted
+    def test_is_between_longitudes_g(self):
+        min_long = 45
+        location_long = -120
+        max_long = -160
+
+        obj = MapObject(location_long=location_long)
+        self.assertFalse(obj.is_between_longitudes(min_long=min_long, max_long=max_long))
+
+    """
+        Test is_between_latitudes()
+    """
+
+    def test_is_between_latitudes_below(self):
+        min_lat = 30
+        location_lat = 10
+        max_lat = 50
+
+        obj = MapObject(location_lat=location_lat)
+        self.assertFalse(obj.is_between_latitudes(min_lat=min_lat, max_lat=max_lat))
+
+    def test_is_between_latitudes_inside(self):
+        min_lat = 30
+        location_lat = 40
+        max_lat = 50
+
+        obj = MapObject(location_lat=location_lat)
+        self.assertTrue(obj.is_between_latitudes(min_lat=min_lat, max_lat=max_lat))
+
+    def test_is_between_latitudes_above(self):
+        min_lat = 30
+        location_lat = 60
+        max_lat = 50
+
+        obj = MapObject(location_lat=location_lat)
+        self.assertFalse(obj.is_between_latitudes(min_lat=min_lat, max_lat=max_lat))
+
+
+    """
+        Test is_valid_longitude()
+    """
+
+    def test_is_valid_longitude_too_high(self):
+        longitude = 181
+        self.assertFalse(MapObject.is_valid_longitude(longitude))
+
+    def test_is_valid_longitude_too_low(self):
+        longitude = -181
+        self.assertFalse(MapObject.is_valid_longitude(longitude))
+
+    def test_is_valid_longitude_high_correct(self):
+        longitude = 180
+        self.assertTrue(MapObject.is_valid_longitude(longitude))
+
+    def test_is_valid_longitude_low_correct(self):
+        longitude = -180
+        self.assertTrue(MapObject.is_valid_longitude(longitude))
+
+
+    """
+        Test is_valid_latitude()
+    """
+
+    def test_is_valid_latitude_too_high(self):
+        latitude = 91
+        self.assertFalse(MapObject.is_valid_latitude(latitude))
+
+    def test_is_valid_latitude_too_low(self):
+        latitude = -91
+        self.assertFalse(MapObject.is_valid_latitude(latitude))
+
+    def test_is_valid_latitude_high_correct(self):
+        latitude = 90
+        self.assertTrue(MapObject.is_valid_latitude(latitude))
+
+    def test_is_valid_latitude_low_correct(self):
+        latitude = -90
+        self.assertTrue(MapObject.is_valid_latitude(latitude))
+
+    """
+        Test are_valid_square_params()
+    """
+
+    def test_are_valid_params_incomplete(self):
+
+        square_params = {
+            'min_lat' : None,
+            'max_lat' : '40.0',
+            'min_long' : '80.0',
+            'max_long' : '90.0'
+        }
+        with self.assertRaises(ValidationError):
+            MapObject.get_as_decimals(square_params)
+
+    def test_are_valid_params_no_numeric(self):
+        square_params = {
+            'min_lat' : 'abcd',
+            'max_lat' : '40.0',
+            'min_long' : '80.0',
+            'max_long' : '90.0'
+        }
+        with self.assertRaises(ValidationError):
+            MapObject.get_as_decimals(square_params)
+
+    def test_are_valid_params(self):
+        square_params = {
+            'min_lat' : '30.0',
+            'max_lat' : '40.0',
+            'min_long' : '80.0',
+            'max_long' : '90.0'
+        }
+
+        square_params_decimals = MapObject.get_as_decimals(square_params)
+        self.assertEqual(square_params_decimals['min_lat'], 30.0)
+        self.assertEqual(square_params_decimals['max_lat'], 40.0)
+        self.assertEqual(square_params_decimals['min_long'], 80.0)
+        self.assertEqual(square_params_decimals['max_long'], 90.0)
