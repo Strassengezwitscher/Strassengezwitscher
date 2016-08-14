4e324bb5de4379a93b4051da78303e373e7d9463
Matthias Bastian <dev@matthias-bastian.de>
2016-08-14 15:40:38 +0200

    fix user-related permission names

--- crowdgezwitscher/users/views.py
+++ crowdgezwitscher/users/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 
 class UserListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
-    permission_required = 'users.view_user'
+    permission_required = 'auth.view_user'
     model = User
     template_name = 'users/list.html'
     context_object_name = 'users'
@@ -16,14 +16,14 @@ class UserListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
 
 
 class UserDetail(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
-    permission_required = 'users.view_user'
+    permission_required = 'auth.view_user'
     model = User
     template_name = 'users/detail.html'
     context_object_name = 'user_data'
 
 
 class UserCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
-    permission_required = 'users.add_user'
+    permission_required = 'auth.add_user'
     model = User
     template_name = 'users/form.html'
     fields = ['username', 'email', 'password', 'groups']
@@ -34,7 +34,7 @@ class UserCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
 
 
 class UserUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
-    permission_required = 'users.change_user'
+    permission_required = 'auth.change_user'
     model = User
     template_name = 'users/form.html'
     context_object_name = 'user_data'
