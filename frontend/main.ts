bf5881c6b8a6b23ee4fa324a4e1c5c8af5573987
Max Bothe <mathebox@gmail.com>
2016-09-12 12:38:41 +0200

    Use @types via npm for typescript type information.

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,6 +1,3 @@
-// tslint:disable-next-line:no-reference
-/// <reference path="../typings/index.d.ts" />
-
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { Config } from "./config/config";
