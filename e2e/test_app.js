9f2dbaec53bdff6cd398f2d9d8b9bd6d8f385e48
Max Bothe <mathebox@gmail.com>
2016-09-14 13:13:04 +0200

    Make toolbar menu more responsive. (#214)
    - Hide links depending on screen size.

--- e2e/test_app.js
+++ e2e/test_app.js
@@ -12,7 +12,7 @@ casper.test.begin('Testing Crowdgezwitscher App (User Frontend)', 1, function(te
     })
 });
 
-casper.test.begin('Testing correct display for toolbar on small ', 6, function(test) {
+casper.test.begin('Testing correct display for toolbar on small screens', 6, function(test) {
     casper.start(settings.frontendUrl);
     casper.viewport(600, 1024);
     casper.waitUntilVisible('cg-app', function then(){
@@ -23,15 +23,51 @@ casper.test.begin('Testing correct display for toolbar on small ', 6, function(t
         this.click('#toolbar-menu-button-small');
         this.clickLabel('Kontakt');
         test.assertUrlMatch(/.*\/contact/,'now on page contact');
-        
+
         this.clickLabel('CrowdgezwitscherMap');
         test.assertUrlMatch(/.*\/map/,'now on page map');
-        
+
         this.click('#toolbar-menu-button-small');
         this.clickLabel('Über uns');
         test.assertUrlMatch(/.*\/about/,'now on page about');
 
         this.click('#toolbar-menu-button-small');
+        this.clickLabel('Spenden');
+        casper.waitForPopup(/.*paypal.me\/strassengezwitscher/, function(){
+            test.assertEquals(this.popups.length, 1);
+        }, function timeout() {
+            test.fail('No new page for donation opened');
+        });
+        casper.withPopup(/.*paypal.me\/strassengezwitscher/, function() {
+            test.assertUrlMatch(/.*paypal.me\/strassengezwitscher/,'now on page: donation');
+        });
+    }, function timeout() {
+        test.fail('Could not find element with tag "cg-app"');
+    });
+    casper.run(function(){
+        test.done();
+    })
+});
+
+
+casper.test.begin('Testing correct display for toolbar on medium screens', 6, function(test) {
+    casper.start(settings.frontendUrl);
+    casper.viewport(900, 1024);
+    casper.waitUntilVisible('cg-app', function then(){
+        this.clickLabel('Impressum');
+        test.assertUrlMatch(/.*\/imprint/,'now on page: imprint');
+
+        this.clickLabel('Kontakt');
+        test.assertUrlMatch(/.*\/contact/,'now on page: contact');
+
+        this.clickLabel('CrowdgezwitscherMap');
+        test.assertUrlMatch(/.*\/map/,'now on page map');
+
+        this.click('#toolbar-menu-button-medium');
+        this.clickLabel('Über uns');
+        test.assertUrlMatch(/.*\/about/,'now on page about');
+
+        this.click('#toolbar-menu-button-medium');
         this.clickLabel('Spenden');
         casper.waitForPopup(/.*paypal.me\/strassengezwitscher/, function(){
             test.assertEquals(this.popups.length, 1);
@@ -55,16 +91,16 @@ casper.test.begin('Testing correct display for toolbar on larger screens ', 6, f
     casper.waitUntilVisible('cg-app', function then(){
         this.clickLabel('Impressum');
         test.assertUrlMatch(/.*\/imprint/,'now on page: imprint');
-        
+
         this.clickLabel('Kontakt');
         test.assertUrlMatch(/.*\/contact/,'now on page: contact');
-        
+
         this.clickLabel('Über uns');
         test.assertUrlMatch(/.*\/about/,'now on page: about');
-        
+
         this.clickLabel('CrowdgezwitscherMap');
         test.assertUrlMatch(/.*\/map/,'now on page: map');
-        
+
         this.clickLabel('Spenden');
         casper.waitForPopup(/.*paypal.me\/strassengezwitscher/, function(){
             test.assertEquals(this.popups.length, 1);
@@ -74,7 +110,7 @@ casper.test.begin('Testing correct display for toolbar on larger screens ', 6, f
         casper.withPopup(/.*paypal.me\/strassengezwitscher/, function() {
             test.assertUrlMatch(/.*paypal.me\/strassengezwitscher/,'now on page: donation');
         });
-        
+
     }, function timeout() {
         test.fail('Could not find element with tag "cg-app"');
     });
