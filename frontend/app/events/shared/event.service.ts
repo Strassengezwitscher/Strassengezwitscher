6aa9465bc8d8ec970917fcba8705607a3ba69e89
Max Bothe <mathebox@gmail.com>
2016-09-30 10:17:12 +0200

    Fetch tweet ids from backend.

--- frontend/app/events/shared/event.service.ts
+++ frontend/app/events/shared/event.service.ts
@@ -7,7 +7,7 @@ import "rxjs/add/observable/throw";
 
 @Injectable()
 export class EventService {
-    private eventPageUrl = "api/events/";
+    private eventBaseUrl = "api/events/";
     private lastEvent: Event = null;
 
     constructor(private http: Http) {}
@@ -16,22 +16,34 @@ export class EventService {
         if (this.lastEvent != null && this.lastEvent.id === id) {
             return Observable.of(this.lastEvent);
         }
-        return this.http.get(this.eventPageUrl + id + "/")
-            .map(this.extractData)
+        return this.http.get(this.eventUrl(id))
+            .map(this.extractEventData)
             .do(event => this.lastEvent = event)
             .catch(this.handleError);
     }
 
     public getTweetIds(event: Event): Observable<string[]> {
-        let tweet1Id = "778596668266647552";
-        let tweet2Id = "777941364206108672";
-        let tweet3Id = "777939034450620416";
-        return Observable.of([tweet1Id, tweet2Id, tweet3Id]);
+        return this.http.get(this.tweetsUrl(event.id))
+            .map(this.extractTweetData)
+            .catch(error => Observable.of([]));
     }
 
-    private extractData(response: Response): Event {
+    private eventUrl(eventId: number): string {
+        return `${this.eventBaseUrl}${eventId}/`;
+    }
+
+    private tweetsUrl(eventId: number): string {
+        return `${this.eventUrl(eventId)}tweets.json`;
+    }
+
+    private extractEventData(response: Response): Event {
         let data = response.json();
         return <Event> data;
+    }
+
+    private extractTweetData(response: Response): string[] {
+        let data = response.json() || [];
+        return <string[]> data;
     }
 
     private handleError(error: any) {
