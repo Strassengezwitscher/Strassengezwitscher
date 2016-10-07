b96d710d9a4a19919098e5217919194372fcd89e
Max Bothe <mathebox@gmail.com>
2016-10-07 16:54:17 +0200

    Rework loading of tweets. (#269)

--- frontend/app/twitter/twitter.module.ts
+++ frontend/app/twitter/twitter.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { JsonpModule } from "@angular/http";
 
 import { TweetComponent } from "./tweet.component";
-import { TwitterService } from "./twitter.service";
 
 @NgModule({
-  imports: [BrowserModule, JsonpModule],
+  imports: [BrowserModule],
   declarations: [TweetComponent],
   exports: [TweetComponent],
-  providers: [TwitterService],
 })
 export class TwitterModule {}
