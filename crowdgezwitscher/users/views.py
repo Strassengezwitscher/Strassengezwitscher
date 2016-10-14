d8e796222682d4fcdeef530a02d90e5983bbb29b
Max Bothe <mathebox@gmail.com>
2016-10-14 20:31:37 +0200

    Use selectizejs for user groups input. (#298)

--- crowdgezwitscher/users/views.py
+++ crowdgezwitscher/users/views.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.forms import ModelForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.models import User
 
+from crowdgezwitscher.widgets import SelectizeSelectMultiple
 from .mixins import NoStaffMixin
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'is_active', 'groups')
+        widgets = {
+            'groups': SelectizeSelectMultiple()
+        }
 
 
 class UserListView(PermissionRequiredMixin, NoStaffMixin, ListView):
@@ -38,7 +49,7 @@ class UserCreate(PermissionRequiredMixin, NoStaffMixin, CreateView):
     permission_required = 'auth.add_user'
     model = User
     template_name = 'users/form.html'
-    fields = ['username', 'email', 'password', 'groups']
+    form_class = UserForm
 
     def form_valid(self, form):
         form.instance.set_password(form.instance.password)
@@ -50,7 +61,7 @@ class UserUpdate(PermissionRequiredMixin, NoStaffMixin, UpdateView):
     model = User
     template_name = 'users/form.html'
     context_object_name = 'user_data'
-    fields = ['username', 'first_name', 'last_name', 'email', 'password', 'is_active', 'groups']
+    form_class = UserForm
 
     def form_valid(self, form):
         form.instance.set_password(form.instance.password)
