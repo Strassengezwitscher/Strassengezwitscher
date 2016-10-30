e848ad9a17d6eeafff344c4fc6cb71972bfb30da
Jan Kossmann <kossmann.jan@gmail.com>
2016-10-30 19:22:59 +0100

    Remove model managers

--- crowdgezwitscher/twitter/models.py
+++ crowdgezwitscher/twitter/models.py
@@ -59,11 +59,10 @@ class TwitterAccount(models.Model):
             while len(tweets) > 0:
                 for tweet in tweets:
                     tweetsCrawled += 1
-                    tweetObj = Tweet.objects.create_tweet(tweet['id_str'], tweet['text'], self)
+                    tweetObj = Tweet.objects.create(tweet_id=tweet['id_str'], content=tweet['text'], account=self)
                     for hashtag in tweet['entities']['hashtags']:
                         hashtag, _ = Hashtag.objects.get_or_create(hashtag_text=hashtag['text'])
                         tweetObj.hashtags.add(hashtag)
-                    tweetObj.save()
                 print "%i tweets crawled - now max_id=%s" % (tweetsCrawled, tweets[-1]['id_str'])
                 try:
                     tweets = twitter.request('statuses/user_timeline', {'count': 200, 'user_id': self.account_id,
@@ -78,27 +77,15 @@ class TwitterAccount(models.Model):
     
 # @receiver(post_save, sender=TwitterAccount, dispatch_uid="get_tweets")
 # def get_tweets(sender, instance, **kwargs):
-    
 
-class HashtagManager(models.Manager):
-    def create_hashtag(self, hashtag_text):
-        hashtag = self.create(hashtag_text=hashtag_text)
-        return hashtag
 
 @python_2_unicode_compatible
 class Hashtag(models.Model):
     hashtag_text = models.CharField(max_length=50, unique=True)
 
-    objects = HashtagManager()
-
     def __str__(self):
         return self.hashtag_text
 
-
-class TweetManager(models.Manager):
-    def create_tweet(self, tweet_id, content, account):
-        tweet = self.create(tweet_id=tweet_id, content=content, account=account)
-        return tweet
 
 @python_2_unicode_compatible
 class Tweet(models.Model):
@@ -106,8 +93,6 @@ class Tweet(models.Model):
     content = models.CharField(max_length=250)
     account = models.ForeignKey(TwitterAccount, on_delete=models.CASCADE)
     hashtags = models.ManyToManyField(Hashtag)
-
-    objects = TweetManager()
 
     def __str__(self):
         return "%s - %s" % (self.account, self.content)
