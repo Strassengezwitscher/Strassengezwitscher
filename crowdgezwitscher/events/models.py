e0cb123c7d3d3552d9fe564a8d38a0ad4e769bbd
Matthias Bastian <dev@matthias-bastian.de>
2016-11-12 15:47:07 +0100

    Admin-UI allows changing files in attachment instances. old file is now deleted from filesystem.

--- crowdgezwitscher/events/models.py
+++ crowdgezwitscher/events/models.py
@@ -6,7 +6,6 @@ import string
 
 from django.urls import reverse
 from django.db import models
-from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.timezone import now as timezone_now
@@ -95,6 +94,18 @@ class Attachment(models.Model):
         return "<Attachment %s for %s>" % (self.name, self.event)
 
 
-@receiver(post_delete, sender=Attachment)
+@receiver(models.signals.post_delete, sender=Attachment)
 def auto_delete_file_on_delete(**kwargs):
+    """Deletes file from filesystem when corresponding `Attachment` instance is deleted."""
     kwargs['instance'].attachment.delete(save=False)
+
+
+@receiver(models.signals.pre_save, sender=Attachment)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """Deletes file from filesystem when corresponding `Attachment` instance is changed."""
+    if not instance.pk: return  # file is created instead of changed
+
+    try:
+        Attachment.objects.get(pk=instance.pk).attachment.delete(save=False)
+    except Attachment.DoesNotExist:
+        return
