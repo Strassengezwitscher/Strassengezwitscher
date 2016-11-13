d590282f5d09844b14ac4f104396db03af66ce97
Christopher Schmidt <christopher.schmidt1989@gmail.com>
2016-11-13 17:41:43 +0100

    added facebookPage-service tets

---
+++ frontend/app/facebook/facebookPage.service.spec.ts
@@ -0,0 +1,61 @@
+import { TestBed, inject } from "@angular/core/testing";
+import { BaseRequestOptions, Http, Response, ResponseOptions } from "@angular/http";
+import { MockBackend } from "@angular/http/testing";
+
+import { FacebookPageService } from "./facebookPage.service";
+
+describe("FacebookPageService", () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                FacebookPageService,
+                MockBackend,
+                BaseRequestOptions,
+                {
+                    provide: Http,
+                    useFactory: (backend, options) => new Http(backend, options),
+                    deps: [MockBackend, BaseRequestOptions],
+                },
+            ],
+        });
+    });
+
+    it("Should return mocked response", inject([MockBackend, FacebookPageService], (mockBackend, service) => {
+        let response = [
+            {
+                "id": 1,
+                "name": "name",
+                "location": "Dresden",
+                "events": [1, 2],
+                "notes": "Note",
+                "facebookId": "10",
+            },
+        ];
+
+        mockBackend.connections.subscribe(connection => {
+            connection.mockRespond(new Response(new ResponseOptions({body: JSON.stringify(response)})));
+        });
+        service.getFacebookPage(1).subscribe(res => {
+            expect(res).toContain({
+                "id": 1,
+                "name": "name",
+                "location": "Dresden",
+                "events": [1, 2],
+                "notes": "Note",
+                "facebookId": "10",
+            });
+        });
+    }));
+
+    it("Should return server error message if Internal Server Error occurs",
+       inject([MockBackend, FacebookPageService], (mockBackend, service) => {
+        mockBackend.connections.subscribe(connection => {
+            connection.mockError(new Error("Internal Server Error 500"));
+        });
+        try {
+            service.getFacebookPage(1).subscribe();
+        } catch (error) {
+            expect(error).toBe("Internal Server Error 500");
+        }
+    }));
+});
