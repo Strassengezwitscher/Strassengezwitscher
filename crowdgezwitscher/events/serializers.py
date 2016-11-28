f7e74e50e73988e0358ebd56e1fa4523a01b2778
Matthias Bastian <dev@matthias-bastian.de>
2016-11-28 14:39:53 +0100

    Use DRF nested relationships for serializing attachments.

--- crowdgezwitscher/events/serializers.py
+++ crowdgezwitscher/events/serializers.py
@@ -7,34 +7,32 @@ import six
 from events.models import Event
 
 
-class AttachmentField(serializers.Field):
-    """Responsible for serialization of attachments referring to an event."""
-    def to_representation(self, attachments):
-        def _build_thumbnail_url(attachment):
-            extension_icon_mapping = {
-                '.pdf':  'img/icon_pdf.png',
-            }
-            default_icon = 'img/icon_file.png'
+class AttachmentSerializer(serializers.Serializer):
+    def to_representation(self, attachment):
+        extension_icon_mapping = {
+            '.pdf':  'img/icon_pdf.png',
+        }
+        default_icon = 'img/icon_file.png'
 
-            if attachment.thumbnail:
-                return settings.MEDIA_URL + six.text_type(attachment.thumbnail)
-            else:
-                extension = os.path.splitext(attachment.name)[1].lower()
-                icon_path = extension_icon_mapping.get(extension, default_icon)
-                return settings.STATIC_URL + six.text_type(icon_path)
+        if attachment.thumbnail:
+            thumbnail_url = settings.MEDIA_URL + six.text_type(attachment.thumbnail)
+        else:
+            extension = os.path.splitext(attachment.name)[1].lower()
+            icon_path = extension_icon_mapping.get(extension, default_icon)
+            thumbnail_url = settings.STATIC_URL + six.text_type(icon_path)
 
-        return [
-            {'name': att.name,
-             'description': att.description,
-             'url': settings.MEDIA_URL + six.text_type(att.attachment),
-             'thumbnail_url': _build_thumbnail_url(att), } for att in attachments
-        ]
+        return {
+            'name': attachment.name,
+            'description': attachment.description,
+            'url': settings.MEDIA_URL + six.text_type(attachment.attachment),
+            'thumbnail_url': thumbnail_url,
+        }
 
 
 class EventSerializer(serializers.ModelSerializer):
     repetitionCycle = serializers.CharField(source='repetition_cycle')
     counterEvent = serializers.BooleanField(source='counter_event')
-    attachments = AttachmentField(source='attachments.all')
+    attachments = AttachmentSerializer(many=True)
 
     class Meta:
         model = Event
