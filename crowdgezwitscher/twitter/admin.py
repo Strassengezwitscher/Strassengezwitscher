33abb8b91b9d1a9108a8e576b908486fc11f28ac
Matthias Bastian <dev@matthias-bastian.de>
2016-12-30 14:40:22 +0100

    show all twitter-related models in Django Admin

--- crowdgezwitscher/twitter/admin.py
+++ crowdgezwitscher/twitter/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from twitter.models import TwitterAccount
+from twitter.models import TwitterAccount, Hashtag, Tweet
 
 
 admin.site.register(TwitterAccount)
+admin.site.register(Hashtag)
+admin.site.register(Tweet)
