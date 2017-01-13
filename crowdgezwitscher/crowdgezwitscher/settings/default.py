0b02c108f820d1cc7d59ff055988d99bd2a34dd3
Max Bothe <mathebox@gmail.com>
2017-01-13 10:59:20 +0100

    Remove line from django project settings.

--- crowdgezwitscher/crowdgezwitscher/settings/default.py
+++ crowdgezwitscher/crowdgezwitscher/settings/default.py
@@ -60,7 +60,6 @@ ROOT_URLCONF = 'crowdgezwitscher.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
