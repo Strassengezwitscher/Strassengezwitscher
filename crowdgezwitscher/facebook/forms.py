39ec6eae7b43cea1708ce48554704868d2ccf0c4
Max Bothe <mathebox@gmail.com>
2017-02-26 16:52:49 +0100

    Fix FacebookPageForm.

--- crowdgezwitscher/facebook/forms.py
+++ crowdgezwitscher/facebook/forms.py
@@ -16,10 +16,14 @@ class FacebookPageForm(forms.ModelForm):
 
     class Meta:
         model = FacebookPage
-        fields = ('name', 'active', 'location_long', 'location_lat', 'location', 'facebook_id', 'notes', 'events')
+        fields = (
+            'name', 'active', 'location_long', 'location_lat', 'location', 'facebook_id', 'notes', 'events',
+            'internal_notes',
+        )
         widgets = {
             'events': SelectizeSelectMultiple(),
             'facebook_id': forms.TextInput(attrs={'class': 'form-control'}),
+            'internal_notes': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
             'location': forms.TextInput(attrs={'class': 'form-control'}),
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'notes': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
